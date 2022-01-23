7c9d183e13126ae8ba073d2c6ca66b0bd976342c
MattDorling <dorling.mj@gmail.com>
2022-01-23 17:46:03 +0000

    Move files instead of copy

--- split-data.py
+++ split-data.py
@@ -7,11 +7,11 @@ train, test = train_test_split(data, train_size=0.75, random_state=1)
 
 for fn in train:
     label_fn = str(fn).split('.')[0] + '.txt'
-    shutil.copyfile(f'data/images/{fn}', f'data/images/train/{fn}')
-    shutil.copyfile(
+    shutil.move(f'data/images/{fn}', f'data/images/train/{fn}')
+    shutil.move(
         f'data/labels/{label_fn}', f'data/labels/train/{label_fn}')
 for fn in test:
     label_fn = str(fn).split('.')[0] + '.txt'
-    shutil.copyfile(f'data/images/{fn}', f'data/images/valid/{fn}')
-    shutil.copyfile(
+    shutil.move(f'data/images/{fn}', f'data/images/valid/{fn}')
+    shutil.move(
         f'data/labels/{label_fn}', f'data/labels/valid/{label_fn}')
